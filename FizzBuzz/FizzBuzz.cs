f6b0a69969059ca1016508c00cdf6eef33ef1eaa
Adrien Caillol <acaillol@sii.fr>
2023-04-17 14:31:25 +0200

    Premier commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,59 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private int _number;
+
+        public string Result
+        {
+            get
+            {
+                string result = "";
+                if (IsFizz())
+                    result += "Fizz";
+
+                if (IsBuzz())
+                    result += "Buzz";
+
+                return String.IsNullOrEmpty(result) ? _number.ToString() : result;
+            }
+        }
+
+        public FizzBuzz(int number)
+        {
+            _number = number;
+        }
+
+        private bool IsFizz()
+        {
+            return ContainsThree() || IsDivisibleByThree();
+        }
+
+        private bool ContainsThree()
+        {
+            string numberString = _number.ToString();
+            return numberString.Contains("3");
+        }
+
+        private bool IsDivisibleByThree()
+        {
+            return _number % 3 == 0;
+        }
+
+        private bool IsBuzz()
+        {
+            return ContainsFive() || IsDivisibleByFive();
+        }
+
+        private bool ContainsFive()
+        {
+            string numberString = _number.ToString();
+            return numberString.Contains("5");
+        }
+
+        private bool IsDivisibleByFive()
+        {
+            return _number % 5 == 0;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                var fizzBuzz = new FizzBuzz(i);
+
+                Console.WriteLine(i.ToString() + " => " + fizzBuzz.Result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
